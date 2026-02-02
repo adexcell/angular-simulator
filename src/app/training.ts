8157fc18642ef7b2390763946c5b6499855b6cd9
abakar_aliev <salachali@gmail.com>
2026-02-03 00:33:21 +0500

    homework 14

---
+++ src/app/training.ts
@@ -0,0 +1,82 @@
+function sum(a: number, b: number): number {
+  return a + b;
+}
+
+
+function stringFormat(str: string, textFormat: string): string {
+  switch (textFormat) {
+    case "uppercase":
+      return str.toUpperCase()
+    case "lowercase":
+      return str.toLowerCase()
+    case "capitalize":
+      return str.charAt(0).toUpperCase() + str.slice(1)
+    default:
+      return ""
+  }
+}
+
+
+function removeChar(str: string, char: string): string {
+  return str.replace(char, "")
+}
+
+
+{
+  let status: "loading" | "success" | "error";
+  let textFormat: "uppercase" | "lowercase" | "capitalize"
+}
+
+interface IUser {
+  id: number;
+  name: string;
+  surname: string;
+  email?: string;
+  age: number;
+}
+
+interface IStudent {
+  group: string;
+  graduate: number;
+}
+
+let users: IUser[] = [
+	{
+		"id": 1,
+		"name": "John",
+		"surname": "Stone",
+		"email": "johnstone@gmail.com",
+		"age": 25
+	},
+	{
+		"id": 2,
+		"name": "Steve",
+		"surname": "Silver",
+		"email": "s.steve@gmail.com",
+		"age": 28
+	},
+	{
+		"id": 3,
+		"name": "Jim",
+		"surname": "River",
+		"email": "jimriver@gmail.com",
+		"age": 24
+	},
+	{
+		"id": 4,
+		"name": "Dwane",
+		"surname": "Mountain",
+		"email": "m.dwane@gmail.com",
+		"age": 29
+	},
+	{
+		"id": 5,
+		"name": "Clark",
+		"surname": "Kent",
+		"email": "clarkkent@gmail.com",
+		"age": 23
+	}
+]
+
+let usersOver25 = users.filter(user => user.age > 25);
+
