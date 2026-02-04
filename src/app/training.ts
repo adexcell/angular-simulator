1b352dd6ec1512e277a9575043fde91e376b5368
abakar_aliev <salachali@gmail.com>
2026-02-04 18:41:10 +0500

    fix issues

--- src/app/training.ts
+++ src/app/training.ts
@@ -3,29 +3,28 @@ function sum(a: number, b: number): number {
 }
 
 
-function stringFormat(str: string, textFormat: string): string {
+function formatString(str: string, textFormat: "uppercase" | "lowercase" | "capitalize"): string {
   switch (textFormat) {
     case "uppercase":
-      return str.toUpperCase()
+      return str.toUpperCase();
     case "lowercase":
-      return str.toLowerCase()
+      return str.toLowerCase();
     case "capitalize":
-      return str.charAt(0).toUpperCase() + str.slice(1)
+      return str.charAt(0).toUpperCase() + str.slice(1);
     default:
-      return ""
+      return str;
   }
 }
 
 
 function removeChar(str: string, char: string): string {
-  return str.replace(char, "")
+  return str.replace(char, "");
 }
 
 
-{
-  let status: "loading" | "success" | "error";
-  let textFormat: "uppercase" | "lowercase" | "capitalize"
-}
+let uploadStatus: "loading" | "success" | "error";
+let textFormat: "uppercase" | "lowercase" | "capitalize"
+
 
 interface IUser {
   id: number;
@@ -42,41 +41,40 @@ interface IStudent {
 
 let users: IUser[] = [
 	{
-		"id": 1,
-		"name": "John",
-		"surname": "Stone",
-		"email": "johnstone@gmail.com",
-		"age": 25
+		id: 1,
+		name: "John",
+		surname: "Stone",
+		email: "johnstone@gmail.com",
+		age: 25
 	},
 	{
-		"id": 2,
-		"name": "Steve",
-		"surname": "Silver",
-		"email": "s.steve@gmail.com",
-		"age": 28
+		id: 2,
+		name: "Steve",
+		surname: "Silver",
+		email: "s.steve@gmail.com",
+		age: 28
 	},
 	{
-		"id": 3,
-		"name": "Jim",
-		"surname": "River",
-		"email": "jimriver@gmail.com",
-		"age": 24
+		id: 3,
+		name: "Jim",
+		surname: "River",
+		email: "jimriver@gmail.com",
+		age: 24
 	},
 	{
-		"id": 4,
-		"name": "Dwane",
-		"surname": "Mountain",
-		"email": "m.dwane@gmail.com",
-		"age": 29
+		id: 4,
+		name: "Dwane",
+		surname: "Mountain",
+		email: "m.dwane@gmail.com",
+		age: 29
 	},
 	{
-		"id": 5,
-		"name": "Clark",
-		"surname": "Kent",
-		"email": "clarkkent@gmail.com",
-		"age": 23
+		id: 5,
+		name: "Clark",
+		surname: "Kent",
+		email: "clarkkent@gmail.com",
+		age: 23
 	}
 ]
 
-let usersOver25 = users.filter(user => user.age > 25);
-
+let usersOver25: IUser[] = users.filter((user: IUser) => user.age > 25);
